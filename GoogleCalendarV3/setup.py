677138c23b38245a34ad8a4a05bd1e0f06ccc63b
ashutosh <ashutosh@eemelabs.com>
2014-06-05 23:28:47 -0700

    Update version numbers for PyPI.

--- GoogleCalendarV3/setup.py
+++ GoogleCalendarV3/setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 
 setup(
     name='GoogleCalendarV3',
-    version='0.1.0',
+    version='0.1.1',
     author='Ashutosh Priyadarshy',
     author_email='static@siftcal.com',
     packages=['google_calendar_v3', 'google_calendar_v3.test'],
     scripts=['bin/example.py'],
-    url='http://pypi.python.org/pypi/GoogleCalendarV3/',
+    url='http://www.github.com/priyadarshy/google-calendar-v3/',
     license='LICENSE.txt',
     description='Python Client for Google Calendar API V3.',
     long_description=open('README.txt').read(),
